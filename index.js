2c5ea8087ee7b661c1a31ec3c7c9e8666a53b03f
mark500tech <mark@500tech.com>
2017-12-26 22:31:55 +0200

    - Created downloadAndUnzip function

--- index.js
+++ index.js
@@ -1,30 +1,13 @@
 const express = require('express');
-const fs = require('fs');
+const constants = require('./constants');
+const dataHandler = require('./dataHandler');
+
 const app = express();
 const port = 8000;
-
-// Constants
-// URLs
-const URL_ADV_ETH = 'https://www.bestchange.ru/advanced-cash-to-ethereum.html';
-const URL_ETH_ADV = 'https://www.bestchange.ru/ethereum-to-advanced-cash.html';
-
-// Classes
-const USD_PRICE_CLASS = '.fs';
 
 app.listen(port);
 console.log("server listening to port: ", port);
 /////////////////////////////////////////////////////////////////////////
 
 // Functions
-function fetchPage(url) {
-  request(url, function (err, response, body) {
-    if (err) {
-      console.log(Date.now() + ' Error while fetching the page')
-      return;
-    }
-
-    const $ = cheerio.load(body);
-    const parseResult = $('.fs').text();
-    console.log('parseResult', parseResult);
-  });
-}
+setInterval(dataHandler.downloadAndUnzip, 5000);
