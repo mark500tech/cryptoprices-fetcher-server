47bbc0703dcaed91bcab5fa1cc7cf1f994c90396
mark500tech <mark@500tech.com>
2017-12-29 13:38:21 +0200

    - Created split file to matrix function - Created currencies file parser

--- dataHandler.js
+++ dataHandler.js
@@ -1,17 +1,18 @@
-import { get } from 'http';
-import { createWriteStream } from 'fs';
+import {get} from 'http';
+import {createWriteStream} from 'fs';
 import AdmZip from 'adm-zip';
-import * as constants from './constants';
+
+import {DATA_FOLDER, DATA_PATH, DOWNLOAD_DATA_URL} from "./constants";
 
 export const downloadAndUnzip = (cb) => {
-  const file = createWriteStream(constants.DATA_PATH);
-  const request = get(constants.DOWNLOAD_DATA_URL, function (response) {
+  const file = createWriteStream(DATA_PATH);
+  const request = get(DOWNLOAD_DATA_URL, function (response) {
     response.pipe(file);
     file.on('finish', function () {
       console.log(new Date() + ' Data file downloaded successfully!');
 
-      const zip = AdmZip(constants.DATA_PATH);
-      zip.extractAllTo(constants.DATA_FOLDER, true);
+      const zip = AdmZip(DATA_PATH);
+      zip.extractAllTo(DATA_FOLDER, true);
       file.close(cb);  // close() is async, call cb after close completes.
     });
   }).on('error', function (err) { // Handle errors
