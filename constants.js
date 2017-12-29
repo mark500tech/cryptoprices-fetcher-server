2703439de4b19d834d6e8068aa7f3eb31cbf4c3a
mark500tech <mark@500tech.com>
2017-12-29 14:08:19 +0200

    - Created downloadAndUnzip function

--- constants.js
+++ constants.js
@@ -2,3 +2,5 @@ export const DOWNLOAD_DATA_URL = 'http://www.bestchange.ru/bm/info.zip';
 export const DATA_FOLDER = './download';
 export const DATA_PATH = `${DATA_FOLDER}/data.zip`;
 export const CURRENCIES_PATH = `${DATA_FOLDER}/bm_cy.dat`;
+export const EXCHANGES_PATH = `${DATA_FOLDER}/bm_exch.dat`;
+export const RATES_PATH = `${DATA_FOLDER}/bm_rates.dat`;
