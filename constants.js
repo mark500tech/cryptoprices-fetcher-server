3f3ac1eda32ed037829819d79a846d48f829a8ce
mark500tech <mark@500tech.com>
2018-01-01 12:14:28 +0200

    - Removed ETC from list of cryptos because sometimes there are no exchanges from ETC to ADV_USD (maybe relevant to some more cryptos)

--- constants.js
+++ constants.js
@@ -17,7 +17,7 @@ export const ID_XRP = '161';
 export const ID_ZEC = '162';
 export const ID_BCH = '172';
 export const CRYPTOS_ID_ARRAY = [
-  ID_ETH, ID_LTC, ID_DASH, ID_XMR, ID_ETC, ID_XRP, ID_ZEC, ID_BCH
+  ID_ETH, ID_LTC, ID_DASH, ID_XMR, ID_XRP, ID_ZEC, ID_BCH
 ];
 export const CURRENCIES_OBJECT = {
   [ID_ADV_USD]: 'Advanced Cash $',
@@ -25,7 +25,6 @@ export const CURRENCIES_OBJECT = {
   [ID_ETH]: 'Etherium',
   [ID_DASH]: 'Dash',
   [ID_XMR]: 'Monero',
-  [ID_ETC]: 'Etherium Classic',
   [ID_XRP]: 'Ripple',
   [ID_ZEC]: 'ZCash',
   [ID_BCH]: 'BitcoinCash',
